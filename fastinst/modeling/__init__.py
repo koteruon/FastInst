287db415999c9ac380e8fb68863044ac65d4bd37
chenluchi420 <chenluchi420@gmail.com>
2024-03-07 04:07:05 +0800

    0307_fastinst

---
+++ fastinst/modeling/__init__.py
@@ -0,0 +1,4 @@
+from .backbone.resnet import build_resnet_vd_backbone
+from .backbone.resnest import build_resnest_backbone
+# from .backbone.resnest_det2 import build_resnest_backbone_det2
+from .meta_arch.fastinst_head import FastInstHead
